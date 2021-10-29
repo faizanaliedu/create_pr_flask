16d3f09c8bea3996951b0162dd39c6591af20a74
Faizan Safdar Ali <faizan.ali@educative.io>
2021-10-29 16:31:45 -0400

    All Prs

--- jira_proj.py
+++ jira_proj.py
@@ -18,24 +18,26 @@ OPTIONS = {
 }
 EDU_REPO_ID = 422668454
 
-def get_ticket_info_from_jira(ticket_number, jira_user, jira_api_key):
-	jira = JIRA(OPTIONS, basic_auth=(jira_user, jira_api_key))
-	issue = jira.issue(ticket_number)
-	return (issue.fields.summary, issue.fields.description)
+def get_ticket_info_from_jira(ticket_number):
+    jira = JIRA(OPTIONS, basic_auth=(app.config['JIRA_USER'], app.config['JIRA_TOKEN']))
+    issue = jira.issue(ticket_number)
+    return (issue.fields.summary, issue.fields.description)
 
 
-def create_pull_request(git_token,summary, description, head_branch, labels, user_name):
-	g = Github(git_token)
-	r = g.get_repo(EDU_REPO_ID)
+def create_pull_request(summary, description, head_branch, labels):
+    g = Github(app.config['GITHUB_TOKEN'])
+    r = g.get_repo(EDU_REPO_ID)
 
-	desciption_template = f"## Description: \n\n {description} \n\n ### Migrations \n\n - None (Add if necessary, and add the run-migration label to the PR.) \n\n ### Author Checklist: \n\n Add an X between [] and remove whitespace to check off. \n\n - [ ] Tests have been added/updated. \n\n - [ ] Relevant documentation has been added/updated (add doc link if applicable). \n\n - [ ] Ran lighthouse tests and added the scores in PR description (if applicable). \n\n - [ ] Appropriate labels have been applied to the PR. Every PR should have at least one label."
+    desciption_template = f"## Description: \n\n {description} \n\n ### Migrations \n\n - None (Add if necessary, and add the run-migration label to the PR.) \n\n ### Author Checklist: \n\n Add an X between [] and remove whitespace to check off. \n\n - [ ] Tests have been added/updated. \n\n - [ ] Relevant documentation has been added/updated (add doc link if applicable). \n\n - [ ] Ran lighthouse tests and added the scores in PR description (if applicable). \n\n - [ ] Appropriate labels have been applied to the PR. Every PR should have at least one label."
 
-	pr = r.create_pull(title=summary, body=desciption_template, head=head_branch, base="master")
+    pr = r.create_pull(title=summary, body=desciption_template, head=head_branch, base="master")
 
-	pr.set_labels(labels)
-	pr.add_to_assignees(user_name)
+    if labels:
+        pr.set_labels(labels)
 
-    return pr.html
+    pr.add_to_assignees(app.config['GITHUB_USER'])
+
+    return pr.html_url
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -47,14 +49,35 @@ def home():
         link = ''
 
         try:
-            summary, description = get_ticket_info_from_jira(jira, app.config['JIRA_USER'], app.config['JIRA_TOKEN'])
-            link = create_pull_request(app.config['GITHUB_TOKEN'], f"{jira}: {summary}", description, github, labels, app.config['GITHUB_USER'])
-        except:
-            return render_template("base.html", message = "Something went wrong")
+            summary, description = get_ticket_info_from_jira(jira)
 
-        return render_template("base.html", message = f"Successfully created PR. Link: {link}")
+            link = create_pull_request(f"{jira}: {summary}", description, github, labels)
+        except:
+            return render_template("home.html", message = "Something went wrong")
+
+        return render_template("home.html", message = "Successfully created PR.", href=link)
     
-    return render_template("base.html")
+    return render_template("home.html")
+
+@app.route("/all_prs", methods=["GET", "POST"])
+def all_prs():
+    if request.method == "POST":
+        g = Github(app.config['GITHUB_TOKEN'])
+        r = g.get_repo(EDU_REPO_ID)
+        user = app.config['GITHUB_USER']
+
+        prs = []
+
+        try:
+            for pull in r.get_pulls(state="open"):
+                if pull.user.login == user:
+                    prs.append((pull.title, pull.html_url))
+        except:
+            return render_template("all_prs.html", message = "Something went wrong")
+
+        return render_template("all_prs.html", message = "Successfully Fetched.", prs=prs)
+    
+    return render_template("all_prs.html")
 
 
 if __name__ == "__main__":
@@ -62,7 +85,5 @@ if __name__ == "__main__":
     app.config['JIRA_TOKEN'] = environ.get('JIRA_TOKEN')
     app.config['GITHUB_TOKEN'] = environ.get('GITHUB_TOKEN')
     app.config['GITHUB_USER'] = environ.get('GITHUB_USER')
-
-    print(app.config['JIRA_TOKEN'])
 
     app.run(debug=True, host="0.0.0.0", port=3000)
