3856b9758912590240af3c77e7eb54448cba3759
Faizan Safdar Ali <faizan.ali@educative.io>
2021-10-29 17:19:49 -0400

    Init

--- jira_proj.py
+++ jira_proj.py
@@ -30,7 +30,7 @@ def create_pull_request(summary, description, head_branch, labels):
 
     desciption_template = f"## Description: \n\n {description} \n\n ### Migrations \n\n - None (Add if necessary, and add the run-migration label to the PR.) \n\n ### Author Checklist: \n\n Add an X between [] and remove whitespace to check off. \n\n - [ ] Tests have been added/updated. \n\n - [ ] Relevant documentation has been added/updated (add doc link if applicable). \n\n - [ ] Ran lighthouse tests and added the scores in PR description (if applicable). \n\n - [ ] Appropriate labels have been applied to the PR. Every PR should have at least one label."
 
-    pr = r.create_pull(title=summary, body=desciption_template, head=head_branch, base="main")
+    pr = r.create_pull(title=summary, body=desciption_template, head=head_branch, base="master")
 
     if labels:
         pr.set_labels(labels)
@@ -48,12 +48,12 @@ def home():
 
         link = ''
 
-        # try:
-        summary, description = get_ticket_info_from_jira(jira)
+        try:
+            summary, description = get_ticket_info_from_jira(jira)
 
-        link = create_pull_request(f"{jira}: {summary}", f"{description} \n https://educative.atlassian.net/browse/{jira}", github, labels)
-        # except:
-        #     return render_template("home.html", message = "Something went wrong")
+            link = create_pull_request(f"{jira}: {summary}", f"{description} \n Jira Link: https://educative.atlassian.net/browse/{jira}", github, labels)
+        except:
+            return render_template("home.html", message = "Something went wrong")
 
         return render_template("home.html", message = "Successfully created PR.", href=link)
     
